81a14a90fe5a2a807cd7489551db8ecac915c003
stuart <stuartwalker041@gmail.com>
2025-06-24 13:44:21 +0300

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! (You can connect this to Formspree or EmailJS)");
+});
