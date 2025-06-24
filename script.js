6af35431ee39b90fc56d1c8370c1b8b73b75dbaa
stuart <stuartwalker041@gmail.com>
2025-06-24 23:20:48 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Message sent! (You can connect this to Formspree or EmailJS)");
+  alert("Message sent!");
 });
